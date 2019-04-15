fbb9de8247ca392af73a5b6b69fa261717d0966c
Caroline Even <caroline.even@rokt.com>
2019-04-15 20:18:46 +1000

    Updates

---
+++ precache-manifest.6bf6f3514166c8f4ccc67bdd08588702.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8d5957654554e975d7699b9a88de65b",
+    "url": "/stamp-duty-calculator/static/media/house.a8d59576.png"
+  },
+  {
+    "revision": "caaf968f80fdd0aaf1845fa74649e65e",
+    "url": "/stamp-duty-calculator/static/media/car.caaf968f.png"
+  },
+  {
+    "revision": "b020b95eb3dea20bba95",
+    "url": "/stamp-duty-calculator/static/js/runtime~main.0224e283.js"
+  },
+  {
+    "revision": "0c8484f48c5e0fcd3306",
+    "url": "/stamp-duty-calculator/static/js/main.31bd9e69.chunk.js"
+  },
+  {
+    "revision": "af61183449ba45e32c9b",
+    "url": "/stamp-duty-calculator/static/js/2.ec4d8b2b.chunk.js"
+  },
+  {
+    "revision": "0c8484f48c5e0fcd3306",
+    "url": "/stamp-duty-calculator/static/css/main.c4bb2f1f.chunk.css"
+  },
+  {
+    "revision": "8001f20ced8db42e00a49d63e4861bc7",
+    "url": "/stamp-duty-calculator/index.html"
+  }
+];
